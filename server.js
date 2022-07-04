e145f355e19039fdf547ad3b304be9d22b2936d7
sunny park <sunnyparkk@gmail.com>
2022-07-04 14:20:16 -0400

    final

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const posts = [
-	{ username: "Mike", post: "this is post 1" },
-	{ username: "Jim", post: "this is post 2" },
+	{
+		username: "Kyle",
+		title: "Post 1",
+	},
+	{
+		username: "Jim",
+		title: "Post 2",
+	},
 ];
 
-app.get("/posts", (req, res) => {
-	res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+	res.json(posts.filter((post) => post.username === req.user.name));
 });
 
-app.get("/login", (req, res) => {
-	res.send("<h1>Login</h1>");
-});
+function authenticateToken(req, res, next) {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+	if (token == null) return res.sendStatus(401);
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+		if (err) {
+			return res.sendStatus(403);
+		} else {
+			req.user = user;
+			next();
+		}
+	});
+}
 
 app.listen(3000);
